b364eb8ebe51d09e67e5c0f62d0ad70b60da99df
milkstard <jmarkcarbs@gmail.com>
2022-08-12 07:17:07 -0400

    Add font files, images, backgrounds

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # webpack-restaurant
 A simple restaurant website that is implemented using webpack.
+
+Photao citation: 
+<ul>
+    <li><a href="https://www.pexels.com/photo/close-up-photo-of-pink-donuts-1982485/">Background</a></li>
+    <li><a href="https://www.pexels.com/photo/woman-covered-her-right-face-with-pink-doughnut-2064662/">ContentMain</a></li>
+</ul>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import './style.css';
 import logoSVG from './delicious-donut-with-chocolate.svg';
+import logoContentMain from './woman-donut.jpg'
 const content = document.createElement('div');
 content.setAttribute('id','content');
 
+const contentHeader = document.createElement('div');
+const contentMain = document.createElement('div');
+
+contentHeader.id = "contentHeader";
+contentMain.id = "contentMain";
 const body = document.querySelector('body');
-body.append(content);
+body.appendChild(content);
+content.append(contentHeader, contentMain);
+// body.append(contentHeader);
+// contentHeader.appendChild(content);
 
 const logo = document.createElement('div');
 const shopName = document.createElement('div');
@@ -14,12 +23,12 @@ logo.classList.toggle('logo');
 shopName.classList.toggle('shopName');
 containerBtn.classList.toggle('containerBtn');
 
-content.append(logo);
-content.append(shopName);
-content.append(containerBtn);
+contentHeader.append(logo,shopName,containerBtn);
 
 const imgLogo = new Image();
+const imgContentMain = new Image();
 imgLogo.src = logoSVG;
+imgContentMain.src = logoContentMain;
 
 logo.appendChild(imgLogo);
 shopName.textContent = "THE DONUT SHOP";
@@ -40,9 +49,15 @@ homeBtnD.append(homeBtn);
 menuBtnD.append(menuBtn);
 contactBtnD.append(contactBtn);
 
-containerBtn.append(homeBtnD);
-containerBtn.append(menuBtnD);
-containerBtn.append(contactBtnD);
+containerBtn.append(homeBtnD, menuBtnD, contactBtnD);
 
+//const homeTxt = document.createElement("div");
+const homeH = document.createElement("h1");
+const homeP = document.createElement("p");
 
+homeH.textContent = "The Donut Shop ..."
+homeP.textContent = "The worlds leading donut shop. With high quality of bread and world class pastry chef.";
 
+contentMain.append(homeH, imgContentMain, homeP);
+
+//contentMain.appendChild(homeTxt);
